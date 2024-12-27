91dfb0c88045b1aab4887ff3d53eea15c930d1c8
Hemanthk408 <g.hemanth0408@gmail.com>
2024-12-27 22:00:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+from sklearn.ensemble import RandomForestClassifier
 
-st.title("🎈 My new app")
+st.title("Welcome to Multi Disease Predictor by using Machine Learning Model Prediction")
+
+
+
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
